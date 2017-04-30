ab4fcfc21625491596dd3c3c02c5ef8610628613
Potatocrunch94 <daniel.pavlekovic@gmail.com>
2017-04-30 13:05:56 +0200

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Daniel Pavlekovic on 30/04/2017.
+//  Copyright © 2017 Daniel Pavlekovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
